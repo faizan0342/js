279e0668d654f08409741fe10faa8e79439b3629
faizan0342 <57402426+faizan0342@users.noreply.github.com>
2020-06-23 22:09:42 +0500

    js

---
+++ app.js
@@ -0,0 +1,492 @@
+//-------------CHAPTER 1 ""ALERTS""---------------//
+//----------------Q1------------------//
+
+//alert("Error! Please Enter A Valid Password")//
+
+//----------------Q2------------------//
+
+//alert("Welcome To JS Land...\nHappy Coding")//
+
+//----------------Q3------------------//
+
+//alert("Welcome To JS Land...")//
+//alert("Happy Coding!"+"\n"+"Prevent This Page From Creating Additional Dialogs")//
+
+//-------------CHAPTER 2 ""VARIABLES FOR STRINGS""---------------//
+//----------------Q1------------------//
+
+//var name=prompt("enter the username")//
+//alert(name)//
+
+//----------------Q2------------------//
+
+//var name=prompt("enter you first name")//
+//var last=prompt("enter your last name")//
+//alert(name+last)//
+
+//----------------Q3------------------//
+
+//var message="HELLO WORLD"//
+//alert(message)//
+
+//----------------Q4------------------//
+
+//var name=prompt("enter your name")//
+//age=prompt("enter your age")//
+//var course=prompt("enter your course name")//
+//alert(name)//
+//alert(age)//
+//alert(course)//
+
+//----------------Q5------------------// adkasdkaslkd;askd;skd;kdk;lks;lkd;lask
+
+//var pix="PIZZA"//
+//var pix1="PIZZ"//
+//var pix2="PIZ"//
+//var pix3="PI"//
+//var pix4="P"//
+//alert(pix  + pix1 + pix2 + pix3 + pix4)//
+
+//----------------Q6------------------//
+
+//var email=prompt("enter your email","example@example.com")//
+//alert("My Email Address is" +  email)//
+
+//----------------Q7------------------//
+
+//var a=prompt("enter your java script book")//
+//alert("I Am Trying To Learn From The Book" + " " + a)//
+
+//----------------Q8------------------//
+
+//var a="Yah! I can write HTML content through Java script"//
+//document.write(a)//
+ 
+//-------------CHAPTER 3 ""VARIABLES FOR NUMBERS""---------------//
+//----------------Q1------------------//
+
+//var age=+prompt("enter your age")//
+//alert("I Am" + " " + age + " " + "Year Old")//
+
+//----------------Q2------------------//
+
+//var a=prompt("Enter Your Birth Of Year")//
+//document.write("My Birth Year is" + " " + a )//
+
+//----------------Q3------------------//
+
+//var name=prompt("enter your name")//
+//var quantity=prompt("enter the quantity")//
+//var product=prompt("enter the product name")//
+//document.write(name + " " + "Ordered" + " " + quantity +" "+ product +" "+ "On XYZ Clothing Store")//
+
+//-------------CHAPTER 5 ""MATH EXPRESSION""---------------//
+//----------------Q1------------------//
+
+//var a=+prompt("enter the first number");//
+//var b=+prompt("enter the second number");//
+//var sum=a+b;//
+//document.write("Sum Of" + " " + a + " And"+ " " + b +" "+ "is"+" "+sum);//
+
+//----------------Q2------------------//
+
+//var a=+prompt("enter the first number");//
+//var b=+prompt("enter the second number");//
+//var sum=a+b;//
+//var subtration=a-b;//
+//var multiply=a*b;//
+//var divided=a\b;//
+//document.write("Sum Of" + " " + a + " And"+ " " + b +" "+ "is"+" "+sum);//
+//document.write("Subtration Of" + " " + a + " And"+ " " + b +" "+ "is"+" "+subtration);//
+//document.write("Multiply Of" + " " + a + " And"+ " " + b +" "+ "is"+" "+multiply);//
+//document.write("Divided Of" + " " + a + " And"+ " " + b +" "+ "is"+" "+divided);//
+
+//----------------Q3------------------//
+
+//var a=+prompt("Enter The Ticket Quantity One Ticket Price 600")//
+//var b=a*600//
+//document.write("Total Cost TO Buy" + " " + a + " " + "Tickets Is Movie is" +" "+ b + " "+ "PKR" )// 
+
+//----------------Q4------------------//
+
+//var a=+prompt("Enter The Table")//
+//for(var i=1;i<=10;i++){//
+//    document.write(a + "x" + i +" ="  +a*i)//
+//    document.write("<br/>")//
+//}
+
+//----------------Q5------------------//
+
+//var Fahranheit=+prompt("Enter Value Fahrenheit")//
+//var value=(Fahranheit-32)*5/9//
+//document.write(value + "C" + " " + "is" + " " + " " + Fahranheit + "F" )//
+//var Celsius=parseFloat(prompt("Enter Value Celsisu"))//
+//var value1=(Celsius*9/5)+32//
+//document.write(value1 + "F" + " " + "is" + " " + Celsisu + "C" )//
+
+//----------------Q7------------------//
+
+//var eng=+prompt("Enter The EngliSh Mark")//
+//var urdu=+prompt("Enter The Urdu Mark")//
+//var math=+prompt("Enter The Math Mark")//
+//var phy=+prompt("Enter The Physics Mark")//
+//var chem=+prompt("Enter The Chemistry Mark")//
+//var total=eng+urdu+math+phy+chem//
+//var per=(total/500)*100//
+//document.write("MARKS SHEET ")//
+//document.write("<br>")//
+//document.write("Total Marks" + " " + total )//
+//document.write("<br>")//
+//document.write("marks Of Obtained"+" "+" 500")//
+//document.write("<br>")//
+//document.write("Percentage" + " " +per)//
+
+//----------------Q8------------------//
+
+//document.write("Convert Currency US Dollar to Pakistani Rupees")//
+//document.write("<br>")//
+//var pd=+prompt("Convert US Dollar to Pakistani Rupees")//
+//var con=pd*104.80//
+//document.write("<br>")//
+//document.write("Total Currency in PKR :" +" "+ con)//
+//document.write("<br>")//
+
+//document.write("convert currency Saudi Riyal to PAkistani Rupees")//
+//document.write("<br>")//
+//var pd1=+prompt("Convert Saudi Riyal to PAkistani Rupees")//
+//var con1=pd1*28//
+//document.write("<br>")//
+//document.write("Total Currency in PKR :" +" "+ con1)//
+
+//----------------Q9------------------//
+
+//document.write("The Geometrizer")//
+//document.write("<br>")//
+//var r=+prompt("Radius Of Circle")//
+//var ans=2*3.142*r//
+//var ans1=3.142*ans*3.142//
+//document.write("<br>")//
+//document.write("Circumference Is"+" "+ ans)//
+//document.write("<br>")//
+//document.write("The Area is :"+" "+ ans1)//
+
+//-------------CHAPTER 6-9 ""ALERT""---------------//
+//----------------Q1------------------//
+
+//var a=prompt("Enter The Number")
+//document.write("RESULT")
+
+//document.write("<br>")
+//document.write("The Value Of A Is" +" "+ a)
+//document.write("<br>")
+//document.write("....................................")
+//document.write("<br>")
+//var a1=++a
+//document.write("The Value Of ++a is :"+" "+ a++ )
+//document.write("<br>")
+//document.write("Now The Value Of A Is :"+" "+ a1 )
+//document.write("<br>")
+//document.write("<br>")
+//var a2=a++
+//document.write("The Value Of a++ is :"+" "+ a1++ )
+//document.write("<br>")
+//document.write("Now The Value Of A Is :"+" "+ a2 )
+//document.write("<br>")
+//document.write("<br>")
+//var a3=--a
+//document.write("The Value Of --a is :"+" "+ --a2 )
+//document.write("<br>")
+//document.write("Now The Value Of A Is :"+" "+ --a3 )
+//document.write("<br>")
+//document.write("<br>")
+//var a4=--a
+//document.write("The Value Of a-- is :"+" "+ a3-- )
+//document.write("<br>")
+//document.write("Now The Value Of A Is :"+" "+ --a4) 
+
+//----------------Q2------------------//
+//var a=+prompt("Enter Value Of A")
+//var b=+prompt("Enter Value Of B")
+//document.write("<br>")
+//document.write("A is"+" "+a)
+//document.write("<br>")
+//document.write("B is"+" "+b)
+//var result=--a - --b + ++b + b--
+//document.write("<br>")
+//document.write("Result is"+" "+result)
+
+//----------------Q3------------------//
+
+//var username=prompt("what's your name")
+//document.write("Hello"+" "+username+" "+"How Are You Doing Today")
+
+//----------------Q4------------------//
+
+//document.write("Subject Total Marks Obtained Marks Percentage")
+//document.write("<br>")
+//eng=+prompt("Enter  English Marks")
+//math=+prompt("Enter Math Marks")
+//urdu=+prompt("Enter Urdu Marks")
+//total=eng+math+urdu
+//per=(total/300)*100
+//document.write("<br>")
+//document.write("English" +  "&nbsp &nbsp &nbsp" +"100" + " &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp"+ eng +" &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp"+ eng+"%")
+//document.write("<br>")
+//document.write("Math" +  "&nbsp &nbsp &nbsp &nbsp &nbsp" +"100" + " &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp"+ math +" &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp"+ math+"%")
+//document.write("<br>")
+//document.write("Urdu" +  "&nbsp &nbsp &nbsp &nbsp &nbsp" +"100" + " &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp"+ urdu +" &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp"+ urdu+"%")
+//document.write("<br>")
+//document.write("........................................................................................")
+//document.write("<br>")
+//document.write("&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp" + "300" +"&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp"+ total + "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp"+ per+"%")
+
+//-------------CHAPTER 6-9 ""USER INPUT & CONDITIONAL STATEMENT""---------------//
+//----------------Q1------------------//
+
+//city=prompt("Enter The City Name")
+//if(city == "karachi"){
+//    alert("Welcome To City Of Lights")
+//}
+
+//----------------Q2------------------//
+
+//gender=prompt("Enter The Gender")
+//if(gender=="male"){
+//    alert("Good Morning Sir")
+//}
+//else if(gender=="female"){
+//    alert("Good Morning Ma'am")
+//}
+//else{
+//    alert("Again Gender")
+//}
+
+//-------------CHAPTER 12-13 ""IF...ELSE & ELSE IF STATEMENT,TESTING SET CONDITION""---------------//
+//----------------Q1------------------//
+
+//var char=prompt("Enter Character")
+//if(char>=65 || char<=90 || char>="A" || char<="Z"){
+//    alert("It is Upper case Leter")
+//}
+//else if(char>=97 || char<=122 || char>="a" || char<="z"){
+//    alert("It is Lower case Leter")
+//}
+//else{
+//   alert("Again Enter Character")
+//}
+
+//----------------Q2------------------//
+
+//var a=5
+//var b=6
+//if(a>=b){
+//    alert("A Is Lager Number")
+//}
+//else(){
+//    alert("B Is Larger NUmber")
+//}
+
+//----------------Q3------------------//
+
+//var number=+prompt("Enter The Number")
+//if(number==0){
+//   alert("It Is Zero")
+//}
+//else if(number < 0){
+//    alert("It It Negative Number")
+//}
+//else{
+//    alert("It Is Positive Number")
+//}
+
+//----------------Q4------------------//
+
+//function isVowel(argument){
+
+//	var text;
+//	var argument = argument.toLowerCase();
+
+//    var vowels = (['a', 'e', 'i', 'o', 'u']);
+
+//    for (var i = 0; i <= vowels.length; i++){
+//        if (argument != vowels[i]) {
+//            continue;
+//        }
+//        return true;
+//    }
+//    return false;
+//}
+
+//var char =prompt("Check Vowel Yes Or Not");
+
+//if (isVowel(char)) {
+//	console.log(char + " is a vowel");
+//} else {
+//	console.log(char + " is not a vowel");
+//}
+
+//----------------Q5------------------//
+
+//var greeting=""
+//var hour=13
+//if(hour<18){
+//    greeting="Good Day"
+//}
+//else{
+//    greeting="Good Evening"
+//}
+//alert(greeting)
+
+//----------------Q6------------------//
+
+//var time=prompt("Enter Time Format Like:1900==7pm")
+//if(time>=0000 && time<1200){
+//    alert("Good Morning")
+//}
+//else if(time>=1200 && time<1700){
+//    alert("Good Afternoon")
+//}
+//else if(time>=1700 && time<2100){
+//    alert("Good Evening")
+//}
+//else if(time>=2100 && time<2359){
+//    alert("Good Night")
+//}
+//else{
+//    alert("Again Enter Time")
+//}
+
+//-------------CHAPTER 14-16 ""ARRAYS""---------------//
+//----------------Q1------------------//
+
+//var object=[]
+//object[0]="shaikh"
+//alert(object)
+
+
+//----------------Q2------------------//
+
+//var arr=["cat","dog","tiger"]
+//console.log(arr[0])
+//console.log(arr[1])
+//console.log(arr[2])
+
+//----------------Q3------------------//
+
+//document.write("QUALIFICATION")
+//document.write("<br>")
+//document.write("<br>")
+//var arr=["SSC","HSC","BCS","BS","BCOM","MS","M.Phil","PHD"]
+//for(var i=0;i<=arr.length-1;i++){
+//    document.write(i+")"+ arr[i])
+//    document.write("<br>")
+//}
+
+//----------------Q4------------------//
+
+//var micheal=prompt("Enter Score Of Michael Below 500")
+//var john=prompt("Enter Score Of John Below 500")
+//var tony=prompt("Enter Score Of Tony Below 500")
+//var total=(micheal/500)*100
+//var total1=(john/500)*100
+//var total2=(tony/500)*100
+//document.write("<br>")
+//document.write("Score Of Michael is"+" "+micheal+" "+"Percentage"+" "+": " + total+"%")
+//document.write("<br>")
+//document.write("Score Of john is"+" "+micheal+" "+"Percentage"+" "+": " + total1+"%")
+//document.write("<br>")
+//document.write("Score Of tony is"+" "+micheal+" "+"Percentage"+" "+": " + total2+"%")
+
+//----------------Q5------------------//
+
+//var arr=[320,230,480,120]
+//a=arr.sort()
+//alert(a)
+
+//----------------Q6------------------//
+
+//document.write("CITIES LIST")
+//document.write("<br>")
+//arr=["karachi,lahore,islamabad,quetta,peshawar"]
+//var copy=arr.slice(2,4)
+//console.log(copy)
+
+//----------------Q7------------------//
+
+//var arr=["this","is","my","cat"]
+//document.write(arr.join(" "))
+
+//-------------CHAPTER 17-20 ""ARRAY AND LOOPS""---------------//
+//----------------Q1------------------//
+
+//for(var i=0;i<10;i++){
+//    document.write(i)
+//    document.write("<br>")
+//   }
+
+//----------------Q2------------------//
+
+//document.write("Multiplication Table")
+//document.write("<br>")
+//var tab=prompt("Enter The Table")
+//var len=prompt("Enter Table Length")
+//for(var i=1;i<=len;i++){
+//    document.write(tab + " " + "x" + " " + i + "=" + tab*i )
+//    document.write("<br>")
+//} 
+
+//----------------Q3------------------//
+
+//var fruit=["apple","banana","mango","orange","strawberry"]
+//document.write("Element at index"+" " + "0" +" "+ "is"+" "+fruit[0])
+//document.write("<br>")
+//document.write("Element at index"+" " + "1" +" "+ "is"+" "+fruit[1])
+//document.write("<br>") 
+//document.write("Element at index"+" " + "2" +" "+ "is"+" "+fruit[2])
+//document.write("<br>") 
+//document.write("Element at index"+" " + "3" +" "+ "is"+" "+fruit[3])
+//document.write("<br>") 
+//document.write("Element at index"+" " + "4" +" "+ "is"+" "+fruit[4]) 
+
+//----------------Q4------------------//
+
+//var arr = [14, 58, 20, 77, 66, 82, 42, 67, 42, 4]
+//var min = Math.min(arr)
+//console.log(min)
+
+//----------------Q5------------------//
+
+ 
+//var gfg = new Array(3); 
+//document.write("Creating 2D array <br>"); 
+//for (var i = 0; i < gfg.length; i++) { 
+//    gfg[i] = []; 
+//} 
+//var h = 0; 
+//var s = [1,2,3,4,5,6,7,8,9];  
+//for (var i = 0; i < 3; i++) { 
+//    for (var j = 0; j < 3; j++) { 
+  
+//        gfg[i][j] = s[h++]; 
+//    } 
+//}  
+//for (var i = 0; i < 3; i++) { 
+//    for (var j = 0; j < 3; j++) 
+  
+//    { 
+//       document.write(gfg[i][j] + " "); 
+//    } 
+//    document.write("<br>"); 
+//} 
+
+//----------------Q6------------------//
+
+//var array = [24,53,78,91,12];
+//var largest= 0;
+
+//for (i=0; i<=largest;i++){
+//    if (array>largest) {
+//        var largest=array[i];
+//    }
+//}
+//console.log(largest);
